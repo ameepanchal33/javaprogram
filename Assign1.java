5e00ffe7130c95d0a0f37092ecc5d7d90c7ab7b4
Amee <amipanchal33@gmail.com>
2023-07-05 22:43:58 +0530

    javaprogram

--- Assign1.java
+++ Assign1.java
@@ -1,7 +1,7 @@
 package basic;
 import java.util.Scanner;
 
-public class GreatestNo {
+public class Assign1 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the number::");
