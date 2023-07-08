1ce9a5a18989dfc66bc27fff8be8c517af96c540
Amee <amipanchal33@gmail.com>
2023-07-09 00:15:09 +0530

    javaprogram

--- Assign7.java
+++ Assign7.java
@@ -1,7 +1,7 @@
 package basic;
 import java.util.Scanner;
 
-public class RtriangleIncreasingNo {
+public class Assign7 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter the no:");
