65ec97e65ccb80a2970dcb4eb469d81817b9045b
Amee <amipanchal33@gmail.com>
2023-07-14 09:59:06 +0530

    javaprogram

--- Assign11.java
+++ Assign11.java
@@ -1,7 +1,7 @@
 package basic;
 import java.util.Scanner;
 
-public class IntegerCompute {
+public class Assign11 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		System.out.print("enter the no:");
