28e2b778f014d72b3953f96f0f3efe868aab221d
Amee <amipanchal33@gmail.com>
2023-07-15 09:44:53 +0530

    javaprogram

--- Assign12.java
+++ Assign12.java
@@ -1,6 +1,6 @@
 package basic;
 
-public class SystemTime {
+public class Assign12 {
  public static void main(String[] args) {
 	 System.out.format("date and of System %tc%n",System.currentTimeMillis());
 	
