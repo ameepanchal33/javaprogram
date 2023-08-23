31dc3cc7e03657bef6cdd1144c7f5382dac89ac2
Amee <amipanchal33@gmail.com>
2023-08-23 23:58:41 +0530

    java program

---
+++ bubble.java
@@ -0,0 +1,27 @@
+package basic;
+
+import java.util.Arrays;
+
+public class bubble {
+	
+	public static void main(String[] args) {
+		
+		int a[]= {4,5,3,1,2};
+//		int n=a.length;
+		System.out.println("arrys before swapping:"+Arrays.toString(a));
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<a.length;j++){
+				if(a[j]>a[j+1]) {
+				int temp=a[j];
+					a[j]=a[j+1];
+				    a[j+1]=temp;
+				}   
+				
+			}
+		}
+		System.out.println("after swapping"+Arrays.toString(a));
+
+	}
+	
+	
+}
